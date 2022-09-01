aecaea86caf589a07481d3ba1762704cae589943
yoon1fe <yoon1fe@naver.com>
2022-09-01 21:22:31 +0900

    init project

---
+++ src/main/kotlin/com/hedoleague/HedoLeagueApplication.kt
@@ -0,0 +1,11 @@
+package com.hedoleague
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HedoLeagueApplication
+
+fun main(args: Array<String>) {
+	runApplication<HedoLeagueApplication>(*args)
+}
---
+++ src/test/kotlin/com/hedoleague/HedoLeagueApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hedoleague
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HedoLeagueApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
