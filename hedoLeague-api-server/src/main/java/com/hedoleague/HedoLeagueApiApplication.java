fc9185ee056e755854bf73e3e65e6d0f268b69a3
Woncheol Yoon <yoon1fe@naver.com>
2023-05-14 01:54:02 +0900

    멀티모듈 구성
    - hedoLeage-api-server
    - hedoLeage-web-server

--- hedoLeague-api-server/src/main/java/com/hedoleague/HedoLeagueApiApplication.java
+++ hedoLeague-api-server/src/main/java/com/hedoleague/HedoLeagueApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HedoLeagueApplication {
+public class HedoLeagueApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HedoLeagueApplication.class, args);
+		SpringApplication.run(HedoLeagueApiApplication.class, args);
 	}
 
 }
--- hedoLeague-core/src/main/java/com/hedoleague/common/util/properties/UrlProperties.java
+++ hedoLeague-core/src/main/java/com/hedoleague/common/util/properties/UrlProperties.java
@@ -6,7 +6,7 @@ import java.nio.charset.Charset;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.util.StringUtils;
+import org.springframework.util.StringUtils;
 
 @Data
 @Configuration
